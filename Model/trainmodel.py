739ce415f37bdb0debab64cdf169475a8db79430
hagar_haytham <hagarhaytham597@gmail.com>
2019-11-01 15:17:25 +0200

    eleven plane encoder

--- Model/trainmodel.py
+++ Model/trainmodel.py
@@ -2,6 +2,8 @@ from dataprocessor import DataProcessor
 from datagenerator import DataGenerator
 from encoder.oneplane import OnePlaneEncoder
 from encoder.sevenplanes import SevenPlaneEncoder
+from encoder.elevenplanes import ElevenPlaneEncoder
+
 # import arch
 import smallarch as arch
 from keras.models import Sequential
@@ -13,12 +15,13 @@ go_board_rows, go_board_cols = 19, 19
 num_classes = go_board_rows * go_board_cols
 num_games = 100
 # encoder = OnePlaneEncoder((go_board_rows, go_board_cols))
-encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
+# encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
+encoder = ElevenPlaneEncoder((go_board_rows, go_board_cols))
 
 processor = DataProcessor(encoder)
 
-# generator = processor.load_go_data('train', num_games,use_generator=True)
-# test_generator =processor.load_go_data('test', num_games,use_generator=True)
+generator = processor.load_go_data('train', num_games,use_generator=True)
+test_generator =processor.load_go_data('test', num_games,use_generator=True)
 
 # from split import Splitter
 # dir = 'dataset/data'
@@ -39,37 +42,37 @@ model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=['accuracy'])
 
 
-X, y = processor.load_go_data(num_samples=1000)
-X = X.astype('float32')
-Y = to_categorical(y, num_classes)
+# X, y = processor.load_go_data(num_samples=1000)
+# X = X.astype('float32')
+# Y = to_categorical(y, num_classes)
 
-network_layers = arch.layers(input_shape)
-model = Sequential()
-for layer in network_layers:
-    model.add(layer)
-model.add(Dense(num_classes, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+# network_layers = arch.layers(input_shape)
+# model = Sequential()
+# for layer in network_layers:
+#     model.add(layer)
+# model.add(Dense(num_classes, activation='softmax'))
+# model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
-model.fit(X, Y, batch_size=128, epochs=100, verbose=1)
+# model.fit(X, Y, batch_size=128, epochs=100, verbose=1)
 
-weight_file = 'weights.hd5'
-model.save_weights(weight_file, overwrite=True)
+# weight_file = 'weights.hd5'
+# model.save_weights(weight_file, overwrite=True)
 
-res =model.predict(X,batch_size=128)
-print(res)
-# epochs = 5
-# batch_size = 128
-# model.fit_generator(
-# generator=generator.generate(batch_size, num_classes), 
-# epochs=epochs,
-# steps_per_epoch=generator.get_num_samples() / batch_size, 
-# validation_data=test_generator.generate(
-# batch_size, num_classes), 
-# validation_steps=test_generator.get_num_samples() / batch_size, 
-# callbacks=[
-# ModelCheckpoint('checkpoints/SevenPlanes_smallarch_model_epoch_{epoch}.h5')
-# ]) 
+# res =model.predict(X,batch_size=128)
+# print(res)
+epochs = 5
+batch_size = 128
+model.fit_generator(
+generator=generator.generate(batch_size, num_classes), 
+epochs=epochs,
+steps_per_epoch=generator.get_num_samples() / batch_size, 
+validation_data=test_generator.generate(
+batch_size, num_classes), 
+validation_steps=test_generator.get_num_samples() / batch_size, 
+callbacks=[
+ModelCheckpoint('checkpoints/ElevenPlanes_smallarch_model_epoch_{epoch}.h5')
+]) 
 
-# model.evaluate_generator(
-# generator=test_generator.generate(batch_size, num_classes),
-# steps=test_generator.get_num_samples() / batch_size)
+model.evaluate_generator(
+generator=test_generator.generate(batch_size, num_classes),
+steps=test_generator.get_num_samples() / batch_size)
